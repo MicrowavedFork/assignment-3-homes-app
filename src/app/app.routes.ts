4edbb1c32c52e34ed9baa7d774e0a46091b43f00
MicrowavedFork <Lemonadeter90@gmail.com>
2026-03-17 12:16:26 -0500

    Added Routing to the angular application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Homes } from './homes/homes';
+import { Details } from './details/details';
+export const routes: Routes = [
+{
+path: '',
+component: Homes,
+title: 'Home page',
+},
+{
+path: 'details',
+component: Details,
+title: 'Home Details'
+},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Homes } from './homes/homes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Homes],
+  imports: [RouterOutlet, Homes, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
